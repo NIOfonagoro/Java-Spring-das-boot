c9754285f4811dfe972a1be60653bce3669f3c91
Nicholas Ofonagoro <nofonagoro@spartaglobal.com>
2019-04-12 15:08:48 +0100

    Added test suite

--- src/test/java/com/boot/DasBootApplicationTests.java
+++ src/test/java/com/boot/DasBootApplicationTests.java
@@ -1,9 +1,13 @@
 package com.boot;
 
+import com.boot.controller.HomeController;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -11,6 +15,9 @@ public class DasBootApplicationTests {
 
     @Test
     public void contextLoads() {
+        HomeController hc = new HomeController();
+        String result = hc.home();
+        assertEquals(result, "Das Boot, reporting for duty!");
     }
 
 }
